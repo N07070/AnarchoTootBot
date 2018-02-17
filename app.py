ad3760b488038625e4e66fb8f9ea622a428f6f4d
N07070 <nelson.perdriau@gmail.com>
2018-02-17 13:58:34 +0100

    Added the logging config

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ def import_config():
     return config_vars
 
 def main():
-    logging.info(""" This program is free software: you can redistribute it and/or modify
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.info("""
+
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -28,8 +31,9 @@ def main():
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
     """)
-    
+
     new_telegram_bot = TeleBot(import_config()[0], int(import_config()[1]), import_config()[2], import_config()[3])
     new_telegram_bot.run()
 
